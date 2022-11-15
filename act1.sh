728c3c70ae5f67217620485693431fdcb6030706
Guangjian Bao <guangjianbao@gmail.com>
2022-11-15 12:09:13 -0500

    create folder and files with act1 file instructions

--- act1.sh
+++ act1.sh
@@ -9,7 +9,9 @@ touch star_wars/empire/darth_vader.txt
 # 4. Use the force (or your echo) to add the text "...heavy breathing..." to the darth_vader.txt file (Don’t remember how to do this? Use the ‘other force’, known as Google!)
 echo "...heavy breathing..." >> star_wars/empire/darth_vader.txt 
 # 5. Inside the empire directory, create a file called emperor_palpatine.txt
-
+cd empire
+touch emperor_palapatine.txt
 # 6. Inside the empire directory, create a directory called death_star
-
+mkdir death_star
 # 7. Move darth_vader.txt into the death_star
+mv darth_vader.txt death_star
